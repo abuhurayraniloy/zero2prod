15ad88325894d46f1f001c54716d306f04f08afe
botfigther02 <abuhurayraniloy02@gmail.com>
2026-09-08 15:34:01 +0600

    added database and did integration test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-//! main.rs
-
+//! main.rs;
+use sqlx::PgPool;
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
-    run(listener)?.await
+    // Panic if we can't read configuration.
+    let configuration = get_configuration().expect("Falied to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Falied to connect to postgres.");
+    // We have removed the hardcoded `8000` - it is now coming from our settings!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
---
+++ src/startup.rs
@@ -0,0 +1,24 @@
+use crate::routes::{health_check, subscribe};
+use actix_web::dev::Server;
+use actix_web::{App, HttpServer, web};
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // Wrap the pool using web::Data, which boils down to an Arc smart pointer
+    let db_pool = web::Data::new(db_pool);
+    // Capture `connection` from surrounding environment
+    let server = HttpServer::new(move || {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions request
+            .route("/subscriptions", web::post().to(subscribe))
+            // Register the connection as part of the application state
+            // Get a pointer copy and attach it to the pointer state
+            .app_data(db_pool.clone())
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
